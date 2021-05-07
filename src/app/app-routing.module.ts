a9cd0de7e2a01813f11c2ba4b580221f1323116a
rusualexrsa <rusualexrsa@gmail.com>
2021-05-07 21:13:12 +0300

    Remove unused and typo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EmptyComponent } from './components/empty/empty.component';
 import { WalletsComponent } from './components/wallets/wallets.component';
 
 const routes: Routes = [
